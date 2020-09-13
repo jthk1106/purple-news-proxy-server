bd5befcf6b24d3b72d3ea2ff951669942b9e1645
Jay <thk116@gmail.com>
2020-09-12 23:29:11 -0700

    proxy functions with all localhost servers

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const request = require('request')
+
+const app = express()
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send('Proxy says hello Oojoo!')
+})
+
+app.get('/cnn', (req, res) => {
+    request(
+        { url: (`http://localhost:2222/cnn`) },
+        (error, response, body) => {
+            if(error || response.statusCode !== 200) {
+                return res.status(500).json({ type: 'error', message: err.message })
+            }
+
+            res.json(JSON.parse(body))
+        }
+    )
+})
+
+app.get('/fox', (req, res) => {
+    request(
+        { url: (`http://localhost:2222/fox`) },
+        (error, response, body) => {
+            if(error || response.statusCode !== 200) {
+                return res.status(500).json({ type: 'error', message: error.message })
+            }
+
+            res.json(JSON.parse(body))
+        }
+    )
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+const PORT = process.env.PORT || 1111
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`)
+})
